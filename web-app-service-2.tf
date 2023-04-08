e5e9a7f855fd504749b3c5985c576ab90883017e
vijaysakhare709 <129182134+vijaysakhare709@users.noreply.github.com>
2023-04-08 19:15:47 +0530

    Update web-app-service-2.tf

--- web-app-service-2.tf
+++ web-app-service-2.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
 resource "azurerm_resource_group" "vijayss" {
   name     = "api-rg-pro"
   location = "North Europe"
